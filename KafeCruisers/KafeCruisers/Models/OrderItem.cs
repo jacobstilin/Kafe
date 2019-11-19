7ac50bb700fd8b1b2346b3069db14a3036b3f77e
Jacob Stilin <stilinj@gmail.com>
2019-11-19 11:02:50 -0600

    Initial Commit

---
+++ KafeCruisers/KafeCruisers/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KafeCruisers.Models
+{
+    public class OrderItem
+    {
+    }
+}
---
+++ KafeCruisers/KafeCruisers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KafeCruisers.Startup))]
+namespace KafeCruisers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
