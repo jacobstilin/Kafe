4504dbc54af7152883a5a87cbc8e299320221a7a
Jacob Stilin <stilinj@gmail.com>
2019-11-27 12:51:10 -0600

    Git ignore

---
+++ KafeCruisers/KafeCruisers/Models/APIKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KafeCruisers.Models
+{
+    public static class APIKeys
+    {
+        public const string GoogleMapsApiKey = "AIzaSyBsmPd4Ot2Vz2iTqxIQEIugk9ORNOnHUD0";
+    }
+}
